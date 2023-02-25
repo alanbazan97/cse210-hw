95a64b381cc40068b5617613a3f499e09648aee3
alanmong97 <baz21001@byui.edu>
2023-02-26 00:03:00 +0100

    modified:   csharp-prep/Prep3/Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        ///Prompt the user for a magic number
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int number = int.Parse(magicNumber); 
+        ///While Loop
+        while (number != guess)
+        {
+            ///Prompt the user for a guess
+            Console.Write("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            int guess = int.Parse(guessNumber);
+            ///Determining if the user needs to guess higher or lower next time, or telling them if they guessed
+            if ( guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)70
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+            Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
