2ec3a73a5b03e90de20e6887c8baddf732d4d9b9
alanmong97 <baz21001@byui.edu>
2023-02-28 18:43:18 +0100

    modified:   prepare/Learning02/Program.cs new file:   prepare/Learning02/Resume.cs new file:   prepare/Learning02/Job.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //For this assigment, I wanted to use my personal jobs informations
+        Job job1 = new Job()
+        job1._company = "Amazon";
+        job1-_jobTitle = "Warehouse Associate";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._company = "Amazon";
+        job2._jobTitle = "operations Supervisor";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+        //Displaying name and all the jobs
+        Resume resume = new Resume();
+        resume._name = "Alan Ronaldo Bazan Monge";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
