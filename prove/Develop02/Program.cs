d55a7efdfa8afb919903f59a4430caf71bc45e7d
alanmong97 <baz21001@byui.edu>
2023-03-06 17:21:41 +0100

    modified:   prove/Develop02/Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -18,6 +18,45 @@ class Program
         {
             action = Choices();
 
+            if (action == 1)
+            {
+                string entryId = GetEntryId();
+                string dateInfo = GetDateTime();
+                string prompt = journalPrompt.GetPrompt();
+
+                JournalEntry entry = new JournalEntry();
+                entry._entryNumber = entryId;
+                entry._dateTime = dateInfo;
+                entry._journalPrompt = prompt;
+                Console.Write($"{prompt}");
+                string userEntry = Console.ReadLine();
+                entry._journalEntry = userEntry;
+                journal._journal.Add(entry);
+            }
+            else if (action == 2)
+            {
+                journal.Display();
+            }
+            else if (action == 3)
+            {
+                journal.CreateJournalFile();
+            }
+            else if (action == 4)
+            {
+                journal.LoadJournalFile();
+            }
+            else if (action == 5)
+            {
+                Console.WriteLine("Thank you for using the Journal App!");
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry, the option you entered is not valid.");
+            }
+        }
+    }
+            /* Using Switch
+
             switch (action) 
             {
                 case 1:
@@ -49,25 +88,34 @@ class Program
                 default:
                     Console.WriteLine("I'm sorry, the option you entered is not valid.");
                     break;
-            }
-        }
-    }
+            }*/
     static int Choices()
     {
         string choices = @"
-Please select one of the following choices:
-1. Write
-2. Display
-3. Save
-4. Load
-5. Quit
-What would you like to do? ";
+        Please select one of the following choices:
+        1. Write
+        2. Display
+        3. Save
+        4. Load
+        5. Quit
+        What would you like to do? ";
+
+        /* Using another method:
+        string choices = "Please select one of the following choices:\n" +
+        "1. Write\n" +
+        "2. Display\n" +
+        "3. Save\n" +
+        "4. Load\n" +
+        "5. Quit\n" +
+        "What would you like to do? "; */
 
         Console.Write(choices);
         string userInput = Console.ReadLine();
         int action = int.Parse(userInput);
         return action;
     }
+
+            
 
     static string GetDateTime()
     {
