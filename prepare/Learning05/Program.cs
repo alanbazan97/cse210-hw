0dc3ec05488ad1928e124906ae00c2125102a949
alanmong97 <baz21001@byui.edu>
2023-03-21 22:56:25 +0100

    modified:   prepare/Learning05/Program.cs 	modified:   prove/Develop04/Activity.cs 	modified:   prove/Develop04/Breathing.cs 	modified:   prove/Develop04/Listing.cs 	modified:   prove/Develop04/Program.cs 	modified:   prove/Develop04/Reflection.cs 	prepare/Learning05/Circle.cs 	prepare/Learning05/Rectangle.cs 	prepare/Learning05/Shape.cs 	prepare/Learning05/Square.cs

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 5.0);
+        Rectangle rectangle = new Rectangle("blue", 4.0, 6.0);
+        Circle circle = new Circle("green", 3.0);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        Console.WriteLine("All shapes:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.Color}");
+            Console.WriteLine($"Shape area: {shape.GetArea()}");
+        }
     }
 }
